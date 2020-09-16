968bc6cbe3fbe181088a5b682053473d405058aa
michael-ayako_BEAST <56703374+michael-ayako@users.noreply.github.com>
2020-09-16 11:15:10 -0500

    Restructured zzzMain Database Code

--- database_code/ZZmain.py
+++ database_code/ZZmain.py
@@ -11,26 +11,74 @@ class database_handler:
     def __init__(self, logger):
         self.logger = logger
 
+    def func_load_events_table(self):
+        try:
+            print(colored("Let's start by loading event data", 'green'))
+            load_events_table(self.logger).__main__()
+        except Exception as excpt:
+            print('Error : {}'.format(excpt))
+            self.logger.warning('Error : {}'.format(excpt))
+
+    def func_load_subject_abrv_to_full_table(self):
+        try:
+            print()
+            print(colored("Loading subject_abrv_to_full table", 'green'))
+            load_subject_abrv_to_full_table(self.logger).__main__()
+        except Exception as excpt:
+            print('Error : {}'.format(excpt))
+            self.logger.warning('Error : {}'.format(excpt))
+
+    def func_load_faculty_table(self):
+        try:
+            print()
+            print(colored("Loading faculty table", 'green'))
+            load_faculty_table(self.logger).__main__()
+        except Exception as excpt:
+            print('Error : {}'.format(excpt))
+            self.logger.warning('Error : {}'.format(excpt))
+
+    def func_load_course_table(self):
+        try:
+            print()
+            print(colored("Loading course table", 'green'))
+            load_course_table(self.logger).__main__()
+        except Exception as excpt:
+            print('Error : {}'.format(excpt))
+            self.logger.warning('Error : {}'.format(excpt))
+
+    def func_load_section_table(self):
+        try:
+            print()
+            print(colored("Loading course table", 'green'))
+            load_section_table(self.logger).__main__()
+        except Exception as excpt:
+            print('Error : {}'.format(excpt))
+            self.logger.warning('Error : {}'.format(excpt))
+
+    def func_load_section_table(self):
+        try:
+            print()
+            print(colored("Loading course table", 'green'))
+            load_section_table(self.logger).__main__()
+        except Exception as excpt:
+            print('Error : {}'.format(excpt))
+            self.logger.warning('Error : {}'.format(excpt))
+
+    def func_load_course_has_faculty_table(self):
+        try:
+            print()
+            print(colored("Loading course_has_faculty table", 'green'))
+            load_course_has_faculty_table(self.logger).__main__()
+        except Exception as excpt:
+            print('Error : {}'.format(excpt))
+            self.logger.warning('Error : {}'.format(excpt))
+
     def __main__(self):
         print()
         print("Loading data to the database")
-        print(colored("Let's start by loading event data", 'green'))
-        load_events_table(self.logger).__main__()
-        print()
-        print(colored("Loading subject_abrv_to_full table", 'green'))
-        load_subject_abrv_to_full_table(self.logger).__main__()
-        print()
-        print(colored("Loading faculty table", 'green'))
-        load_faculty_table(self.logger).__main__()
-        print()
-        print(colored("Loading course table", 'green'))
-        load_course_table(self.logger).__main__()
-        print()
-        print(colored("Loading section table", 'green'))
-        load_section_table(self.logger).__main__()
-        print()
-        print(colored("Loading section table", 'green'))
-        load_section_table(self.logger).__main__()
-        print()
-        print(colored("Loading course_has_faculty table", 'green'))
-        load_course_has_faculty_table(self.logger).__main__()
+        self.func_load_events_table()
+        self.func_load_subject_abrv_to_full_table()
+        self.func_load_faculty_table()
+        self.func_load_course_table()
+        self.func_load_section_table()
+        self.func_load_course_has_faculty_table()
